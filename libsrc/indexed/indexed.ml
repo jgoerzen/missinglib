a378b2a6c5b0e7ffba73c88642defa0120e352c9
John Goerzen <jgoerzen@complete.org>
2004-04-29 14:42:26 -0500

    Checkpointing
    Keywords:
    
    (jgoerzen@complete.org--projects/missinglib--head--0.0--patch-15)
    
    darcs-hash:20040429194226-c2a52-4955bd76f80f5b529fde6ac2a5de92661468ab57.gz

--- libsrc/indexed/indexed.ml
+++ libsrc/indexed/indexed.ml
@@ -1,42 +1,54 @@
 (* arch-tag: support for indexed types
 Copyright (C) 2004 John Goerzen 
 *)
-
-class virtual ['b] indexed (init:'a) = 
+class type ['a, 'b] indexed_t = 
+  object 
+    method sub : int -> int -> ('a, 'b) indexed_t
+    method length : int
+    method prepend: 'b -> ('a, 'b) indexed_t
+    method extend: 'a -> ('a, 'b) indexed_t
+    method get: 'a
+    method iter: ('b -> unit) -> unit
+    (*
+    method map: 'd.  ('b -> 'd) -> 'd indexed_t
+    method fold_left: 'd.  ('d -> 'b -> 'd) -> 'd -> 'd  *)
+    method sort: ('b -> 'b -> int) -> ('a, 'b) indexed_t
+    method set: 'a -> unit
+  end
+;;
+class virtual ['a, 'b] indexed (init:'a) = 
   object (self: 'c)
     val mutable contents = (init:'a)
     method private copyhelper data = 
       let newobj = Oo.copy self in
       newobj#set data;
       newobj
-    method virtual sub : int -> int -> 'c
-    method virtual length : int
-    method virtual prepend: 'b -> 'c
-    method virtual extend: 'a -> 'c
     method get = contents
-    method virtual iter: ('b -> unit) -> unit
-    method virtual map: 'd 'e.  ('b -> 'd) -> 'd indexed
+    (*
+    (* method virtual map: 'd 'e.  ('b -> 'd) -> ('e, 'd) indexed_t *)
+    method virtual map: 'd 'e.  ('b -> 'd) -> 'e
     method virtual fold_left: 'd.  ('d -> 'b -> 'd) -> 'd -> 'c
-    method virtual sort: ('b -> 'b -> int) -> 'c 
+    *)
     method set x = contents <- x  
   end
 ;;
-
-class ['b] indexedarray init =
+class ['z] indexedarray (init:'z array) =
   object (self: 'c)
-    inherit ['b] indexed init
-    constraint 'a = 'b array
-    method sub x y = self#copyhelper (Array.sub contents x y)
+    inherit ['z array, 'z] indexed init
+    method sub x y = (self#copyhelper (Array.sub contents x y) :> ('z array, 'z)
+    indexed_t)
     method length = Array.length contents
-    method prepend newitem = self#copyhelper (
+    method prepend newitem = (new indexedarray (
       let a = Array.make 1 newitem in
-      Array.append a contents)
-    method extend newitems = self#copyhelper (Array.append contents newitems)
+      Array.append a contents) :> ('z array, 'z) indexed_t)
+    method extend  = fun newitems -> ((self#copyhelper (Array.append contents
+    newitems)) :> ('z array, 'z) indexed_t)
     method iter f = Array.iter f contents
     method map f = new indexedarray (Array.map f contents) 
     (*
     method fold_left f x = self#copyhelper(Array.fold_left f x contents) *)
-    method sort f = self#copyhelper (
+    method sort f = (self#copyhelper (
       let newobj = Array.copy contents in Array.sort f newobj; newobj)
+      :> ('z array, 'z) indexed_t)
   end
 ;;
