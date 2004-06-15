05deb504f0e615df3072f2c68637fcf9fa333878
John Goerzen <jgoerzen@complete.org>
2004-06-15 16:52:50 -0500

    Start of compose support
    Keywords:
    
    (jgoerzen@complete.org--projects/missinglib--head--0.0--patch-29)
    
    darcs-hash:20040615215250-c2a52-c1baa30bd75b99d358f3240b1abdcd2869b8bcce.gz

---
+++ libsrc/compose/composeoper.mli
@@ -0,0 +1,28 @@
+(* arch-tag: Composition operator interface
+Copyright (C) 2004 John Goerzen <jgoerzen@complete.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*)
+
+(** Compose
+
+This module provides basic support for functions supporting function
+composition. *)
+
+(** Calling (f % g) x is equivolent to calling f(g(x)). *)
+val (%): ('a -> 'b) -> ('c -> 'a) -> 'c -> 'b
+
+(** This will apply a function. *)
+val ($): ('a -> 'b) -> 'a -> 'b
